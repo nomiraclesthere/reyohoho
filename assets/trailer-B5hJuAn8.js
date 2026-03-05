d7d2c60f6abcc15850473992908eecc38b996836
dav2010ID <dav2010ID@users.noreply.github.com>
2026-03-05 16:06:42 +0000

    deploy: 66668b124c62ed7b424f38f25eb80664e82dd965

--- assets/trailer-B5hJuAn8.js
+++
@@ -1,1 +0,0 @@
-import{aJ as e}from"./index-BER2O4Me.js";const t=e("trailer",{state:()=>({areTrailersActive:!0}),actions:{activateTrailers(){this.areTrailersActive=!0},deactivateTrailers(){this.areTrailersActive=!1},toggleTrailers(){this.areTrailersActive=!this.areTrailersActive}},persist:!0});export{t as u};
