72d1215c356d6660bc82b5034dd77c259873b6bd
dav2010ID <dav2010ID@users.noreply.github.com>
2026-03-14 20:07:30 +0000

    deploy: 4d1aed7a9803d0ba7d253537bcc73beb8c9f63d5

---
+++ assets/trailer-legacy-Cgl6qqoQ.js
@@ -0,0 +1,1 @@
+System.register(["./app-legacy-DVJs32dd.js"],function(e,r){"use strict";var t;return{setters:[e=>{t=e.aL}],execute:function(){e("u",t("trailer",{state:()=>({areTrailersActive:!0}),actions:{activateTrailers(){this.areTrailersActive=!0},deactivateTrailers(){this.areTrailersActive=!1},toggleTrailers(){this.areTrailersActive=!this.areTrailersActive}},persist:!0}))}}});
