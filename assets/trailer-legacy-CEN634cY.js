aa8561181d63ec2dc7299a36b1c5142864212379
dav2010ID <dav2010ID@users.noreply.github.com>
2026-03-14 14:05:47 +0000

    deploy: 1482664f3a6727deb6501df8f461a28a78184ee1

--- assets/trailer-legacy-CEN634cY.js
+++
@@ -1,1 +0,0 @@
-System.register(["./app-legacy-Buk0VKkV.js"],function(e,r){"use strict";var t;return{setters:[e=>{t=e.aI}],execute:function(){e("u",t("trailer",{state:()=>({areTrailersActive:!0}),actions:{activateTrailers(){this.areTrailersActive=!0},deactivateTrailers(){this.areTrailersActive=!1},toggleTrailers(){this.areTrailersActive=!this.areTrailersActive}},persist:!0}))}}});
