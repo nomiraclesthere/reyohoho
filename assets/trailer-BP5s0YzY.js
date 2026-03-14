6a1b6200742616e660e3890d2d5b48ca795ae01e
dav2010ID <dav2010ID@users.noreply.github.com>
2026-03-14 14:26:58 +0000

    deploy: 27cfd7f0920cde2f49031b9a564052ad0ff10e16

---
+++ assets/trailer-BP5s0YzY.js
@@ -0,0 +1,1 @@
+import{aI as e}from"./app-CLB30-zr.js";const t=e("trailer",{state:()=>({areTrailersActive:!0}),actions:{activateTrailers(){this.areTrailersActive=!0},deactivateTrailers(){this.areTrailersActive=!1},toggleTrailers(){this.areTrailersActive=!this.areTrailersActive}},persist:!0});export{t as u};
